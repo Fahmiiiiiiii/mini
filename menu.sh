48f84876cbbeee7c8ecfc2105038a1408772cdcc
Fahmiiiiiiii <100488113+Fahmiiiiiiii@users.noreply.github.com>
2022-08-27 06:30:04 +0000

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,145 +1,221 @@
 #!/bin/bash
-GREEN='\033[0;32m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m'
-yl='\e[32;1m'
-bl='\e[36;1m'
-gl='\e[32;1m'
-rd='\e[31;1m'
-mg='\e[0;95m'
-blu='\e[34m'
-op='\e[35m'
-or='\033[1;33m'
-bd='\e[1m'
-color1='\e[031;1m'
-color2='\e[34;1m'
-color3='\e[0m'
+# Menu For Script
+# Edition : Stable Edition V1.0
+# Auther  : Fahmi Store
+# (C) Copyright 2021-2022 By Fahmi Hdyt
+# =========================================
+
+# // Exporting Language to UTF-8
+export LC_ALL='en_US.UTF-8'
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+export LC_CTYPE='en_US.utf8'
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+# // Exporting URL Host
+export Server_URL="autosc.me/aio"
+export Server_Port="443"
+export Server_IP="underfined"
+export Script_Mode="Stable"
+export Auther="FsidVPN"
+
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+
+# // Exporting IP Address
+export IP=$( curl -s https://ipinfo.io/ip/ )
+
+freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+	tram=$( free -m | awk 'NR==2 {print $2}' )
+	swap=$( free -m | awk 'NR==4 {print $2}' )
+	ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+#CITY=$(curl -s ipinfo.io/city )
+
+# // Exporting RED BG
+export RED_BG='\e[44m'
+# ==========================================
+MYIP=$(curl -sS ipv4.icanhazip.com)
+#########################
+IZIN=$(curl -sS https://raw.githubusercontent.com/Fahmiiiiiiii/access/main/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+exit 0
+fi
+#EXPIRED
+expired=$(curl -sS https://raw.githubusercontent.com/Fahmiiiiiiii/access/main/ip | grep $MYIP | awk '{print $3}')
+echo $expired > /root/expired.txt
+today=$(date -d +1day +%Y-%m-%d)
+while read expired
+do
+	exp=$(echo $expired | curl -sS https://raw.githubusercontent.com/Fahmiiiiiiii/access/main/ip | grep $MYIP | awk '{print $3}')
+	if [[ $exp < $today ]]; then
+		geo="\033[1;31mExpired\033[0m"
+        else
+        geo=$(curl -sS https://raw.githubusercontent.com/Fahmiiiiiiii/access/main/ip | grep $MYIP | awk '{print $3}')
+	fi
+done < /root/expired.txt
+rm /root/expired.txt
+Name=$(curl -sS https://raw.githubusercontent.com/Fahmiiiiiiii/access/main/ip | grep $MYIP | awk '{print $2}')
 clear
+# // SSH Websocket Proxy
+ssh_ws=$( systemctl status ws-stunnel | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $ssh_ws == "running" ]]; then
+    status_ws="${GREEN}ON${NC}"
+else
+    status_ws="${RED}OFF${NC}"
+fi
+
+# // nginx
+nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $nginx == "running" ]]; then
+    status_nginx="${GREEN}ON${NC}"
+else
+    status_nginx="${RED}OFF${NC}"
+fi
+
+# // SSH Websocket Proxy
+xray=$( systemctl status xray | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $xray == "running" ]]; then
+    status_xray="${GREEN}ON${NC}"
+else
+    status_xray="${RED}OFF${NC}"
+fi
+
+clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;41;36m                 VPS / System Information                 \E[0m${NC}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "Sever Uptime        = $( uptime -p  | cut -d " " -f 2-10000 ) "
+#echo -e "ISP                 = $ISP "
+#echo -e "City                = $CITY "
+echo -e "Operating System    = $( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g' ) ( $( uname -m) )"
+echo -e "Total Amount RAM    = $tram MB"
+echo -e "Current Domain      = $( cat /etc/xray/domain )"
+echo -e "Server IP           = ${IP}"
+echo -e "Current Time        = $( date -d "0 days" +"%d-%m-%Y | %X" )"
 
 echo -e ""
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-DOMAIN=$(cat /etc/xray/domain)
-MYIP=$(curl -s ipinfo.io/ip )
-CITY=$(curl -s ipinfo.io/city )
-up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
-tram=$( free -m | awk 'NR==2 {print $2}' )
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " [ SSH WebSocket${NC} : ${status_ws} ]   [ XRAY${NC} : ${status_xray} ]   [ NGINX${NC} : ${status_nginx} ]"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e " ⚡$bd ISP Name          ${color1} •${color3}$bd $ISP"
-echo -e " ⚡$bd City              ${color1} •${color3}$bd Singapore"
-echo -e " ⚡$bd Total RAM         ${color1} •${color3}$bd $tram MB"
-echo -e " ⚡$bd IP VPS            ${color1} •${color3}$bd $MYIP"
-echo -e " ⚡$bd DOMAIN VPS        ${color1} •${color3}$bd $DOMAIN"
-echo -e " ⚡$bd Waktu Aktif       ${color1} •${color3}$bd $up"
-echo -e " ⚡$bd Client Name       ${color1} •${color3}$bd FAHMI STORE"
-echo -e " ⚡$bd Expiry script     ${color1} •${color3}$bd LIFETIME"
-echo -e " ${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e " \E[44;1;39m                     ⇱ MENU  SSH ⇲                                 \E[0m"
-echo -e " ${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo ""
-echo -e " [${green}01${NC}]${color1} •${color3}$bd Add User SSH  (${color2}menu-ssh${color3})
-echo -e " [${green}02${NC}]${color1} •${color3}$bd SSH WS Enable (${color2}menu-sshws${color3})
-echo -e " [${green}03${NC}]${color1} •${color3}$bd Cek User SSH (${color2}menu-cekssh${color3})
-echo -e " [${green}04${NC}]${color1} •${color3}$bd Delete User SSH (${color2}menu-delssh${color3})
-echo -e " [${green}05${NC}]${color1} •${color3}$bd Renew SSH Account (${color2}menu-renewssh${color3})
-echo -e " [${green}06${NC}]${color1} •${color3}$bd Check User Login (${color2}menu-userlogin${color3})
-echo ""
+echo -e "[${GREEN}01${NC}]${RED} •${NC} Add User SSH$NC         [${GREEN}11${NC}]${RED} • ${NC}Cek Usage"
+echo -e "[${GREEN}02${NC}]${RED} •${NC} Member SSH $NC          [${GREEN}12${NC}]${RED} • ${NC}Cek User Xray $NC"
+echo -e "[${GREEN}03${NC}]${RED} •${NC} Cek User SSH$NC         [${GREEN}13${NC}]${RED} • ${NC}Del User Xray $NC"
+echo -e "[${GREEN}04${NC}]${RED} •${NC} Del User SSH$NC         [${GREEN}14${NC}]${RED} • ${NC}Renew User Xray $NC"
+echo -e "[${GREEN}05${NC}]${RED} •${NC} Renew SSH $NC           [${GREEN}15${NC}]${RED} • ${NC}Add Domain $NC"
+echo -e "[${GREEN}06${NC}]${RED} •${NC} Speedtest Server $NC    [${GREEN}16${NC}]${RED} • ${NC}Cert V2ray $NC"
+echo -e "[${GREEN}07${NC}]${RED} •${NC} Add Vmess Account $NC   [${GREEN}17${NC}]${RED} • ${NC}Backup Data VPS $NC"
+echo -e "[${GREEN}08${NC}]${RED} •${NC} Add Vless Account$NC    [${GREEN}18${NC}]${RED} • ${NC}Restore Data VPS $NC"
+echo -e "[${GREEN}09${NC}]${RED} •${NC} Add Trojan Account$NC   [${GREEN}19${NC}]${RED} • ${NC}Running Service$NC"
+echo -e "[${GREEN}10${NC}]${RED} •${NC} Cek Bandwidth $NC       [${GREEN}20${NC}]${RED} • ${NC}Reboot Server $NC"
+echo -e " ${RED}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;41;36m                      FAHMI STORE                         \E[0m${NC}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "${GREEN}CLIENT NAME${NC}    ${RED}•$NC $Name"
+echo -e "${GREEN}SCRIPT EXPIRED${NC} ${RED}•$NC $exp"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
 
-echo -e ""
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                     ⇱ XRAY MENU ⇲                              \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e ""
-echo -e "[${green}07${NC}]${color1} •${color3}$bd Add Vmess Account (${color2}add-vmess${color3})
-echo -e "[${green}8${NC}]${color1} •${color3}$bd Add Vless Account (${color2}add-vless${color3})
-echo -e "[${green}9${NC}]${color1} •${color3}$bd Add Trojan Account (${color2}add-trojan${color3})
-echo -e "[${green}10${NC}]${color1} •${color3}$bd Add Shadowsock (${color2}add-shadowsock${color3})
-echo -e "[${green}11${NC}]${color1} •${color3}$bd Cek User Xray (${color2}cek-user${color3})
-echo -e "[${green}12${NC}]${color1} •${color3}$bd Dell User Xray (${color2}dell-user${color3})
-echo -e "[${green}13${NC}]${color1} •${color3}$bd Renew User Xray (${color2}cek-user${color3})
-echo ""
+read -p "Input Your Choose ( 1-20 ) : " choosemu
 
-echo -e ""
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                     ⇱ MENU  OPTIONS ⇲                        \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e ""
-echo -e "[${green}14${NC}]${color1} •${color3}$bd Tambahkan Domain (${color2}add-host${color3})
-echo -e "[${green}15${NC}]${color1} •${color3}$bd Generate SSL (${color2}gen-ssl${color3})
-echo -e "[${green}16${NC}]${color1} •${color3}$bd Backup Account (${color2}bckup-user${color3})
-echo -e "[${green}17${NC}]${color1} •${color3}$bd Restore Account (${color2}restore-data${color3})
-echo -e "[${green}18${NC}]${color1} •${color3}$bd PANEL BOT (${color2}panel${color3})
-echo -e "[${green}00${NC}]${color1} •${color3}$bd UNTUK KEMBALI KE MENU (${color2}menu${color3})
-echo ""
 
-echo -e ""
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                     ⇱ FAHMI STORE ⇲                       \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo ""
- read -p "  Select menu :  " menu
-echo -e   ""
-case $menu in
-1)
-usernew
-;;
-2)
-sshws
-;;
-3)
-cek
-;;
-4)
-hapus
-;;
-5)
-renew
-;;
-6)
-member
-;;
-7)
-add-ws
-;;
-8)
-add-vless
-;;
-9)
-add-tr
-;;
-10)
-add-ssws
-;;
-11)
-cek-user
-;;
-12)
-del-user
-;;
-13)
-renew-xray
-;;
-14)
-add-host
-;;
-15)
-crtv2ray
-;;
-16)
-bckp
-;;
-17)
-restore
-;;
-18)
-xolpanel
-0 | 00)
-menu
-;;
-*)
-exit
-;;
+case $choosemu in
+    1) # >> CREATE SSH
+        addssh
+    ;;
+    2) # >> ON OFF SSH WS
+        member
+    ;;
+
+    # R
+    3) # >> CEK SSH
+        cek
+    ;;
+    4) # >> HAPUS AKUN
+        hapus
+    ;;
+    5) # >> ALL Service
+        renew
+    ;;
+    6) # >> Speedtest
+        speedtest
+    ;;
+    7) # >> Ram Usage
+        addws
+    ;;
+    8) # >> Bandiwdth usage
+        clear
+        addvless
+    ;;
+    9) # >> Change Timezone
+        addtr
+        ;;
+    10) # >> Change Timezone
+        bw
+    ;;
+    11) # // Change License
+        cekusage
+    ;;
+    12) # >> Change domain
+        cekws
+    ;;
+    13) # // Renew SSL Cert
+        delws
+    ;;
+    14) # // Add mail
+        renewws
+    ;;
+    15) # // Backup
+        addhost
+    ;;
+    16) # // Restore
+        crt
+    ;;
+    17) # // Create SSL For Stunnel
+        backup
+    ;;
+    18) # // OpenVPN
+        restore
+    ;;
+    19) # // Service VPS
+        running
+    ;;
+    20) # >> Change Timezone
+        reboot
+        ;;
+    *) # >> Wrong Select
+        echo -e "${EROR} SALAH SAYANG !!!"
+        sleep 1
+        menu
+    ;;
 esac
